70c520802c7bb328a967be5f81dbfd597aa6637d
Aiko Vandeputte <aiko@aikovdp.me>
2024-01-23 23:22:51 +0100

    Shade cloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-library`
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     id("xyz.jpenilla.run-paper") version "2.2.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.rokucraft"
@@ -11,7 +12,7 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    maven("https://jitpack.io" )
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -19,7 +20,7 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.11.5")
 
-    library("cloud.commandframework:cloud-paper:2.0.0-SNAPSHOT")
+    implementation("cloud.commandframework:cloud-paper:2.0.0-SNAPSHOT")
 
     library("com.google.dagger:dagger:2.50")
     annotationProcessor("com.google.dagger:dagger-compiler:2.50")
@@ -41,6 +42,17 @@ bukkit {
 }
 
 tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        archiveClassifier = ""
+        listOf(
+            "cloud.commandframework",
+            "io.leangen"
+        ).forEach { relocate(it, "${rootProject.group}.${rootProject.name}.lib.$it") }
+    }
+
     runServer {
         minecraftVersion("1.19.4")
         downloadPlugins {
